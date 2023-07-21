04460da55f2d18db080c2c0b64db1e7df065c344
Emanuel <62862654+Emaarque@users.noreply.github.com>
2023-07-21 10:57:40 -0300

    Update Envio_de_Whatsapps.py

--- Envio_de_Whatsapps.py
+++ Envio_de_Whatsapps.py
@@ -4,9 +4,9 @@ from tkinter import filedialog, messagebox
 from tkinter.ttk import Progressbar
 import webbrowser
 import time
-#import funciones_pct as fn
-#from browser import browser,comprobar_whatsapp
-#import OpenFirefox as of
+import funciones_pct as fn
+from browser import browser,comprobar_whatsapp
+import OpenFirefox as of
 import os
 import sys  
 
